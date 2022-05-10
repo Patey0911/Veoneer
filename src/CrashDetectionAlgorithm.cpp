025f73a4ba2f23c1ca00b5022919b9141937b368
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2022-05-10 14:02:22 +0300

    Update CrashDetectionAlgorithm.cpp

--- src/CrashDetectionAlgorithm.cpp
+++ src/CrashDetectionAlgorithm.cpp
@@ -1,36 +1,49 @@
 #include "CrashDetectionAlgorithm.h"
+#include "EEPROM.h"
 void CrashDetectionAlgorithm_Init();
 
 void CrashDetectionAlgorithm_GetCrashType(char *mesaj_crashtype)
 {
+    int cod;
     mesaj_crashtype=(char*)malloc(20*sizeof(char));
-    if(accX<(-0.5))
+    EEPROM.write(1,0);
+    if(accX<(-1))
     {
         const char mes[15]="BACK CRASH";
         strcpy(mesaj_crashtype,mes);
         Serial.println(mesaj_crashtype);
+        EEPROM.write(1,1);
         delay(10000);
     }
-    else if(accX>0.5)
+    else if(accX>1)
     {
         const char mes[15]="FRONT CRASH";
         strcpy(mesaj_crashtype,mes);
         Serial.println(mesaj_crashtype);
+        EEPROM.write(1,2);
         delay(10000);
     }   
-    if(accY>0.5)
+    if(accY>1)
     {
         const char mes[15]="LEFT CRASH";
         strcpy(mesaj_crashtype,mes);
         Serial.println(mesaj_crashtype);
+        EEPROM.write(1,3);
         delay(10000);
     }
-    else if(accY<(-0.5))
+    else if(accY<-1)
     {
         const char mes[15]="RIGHT CRASH";
         strcpy(mesaj_crashtype,mes);
         Serial.println(mesaj_crashtype);
+        EEPROM.write(1,4);
         delay(10000);
+    }
+    cod=EEPROM.read(1);
+    if(cod>0)
+    {
+    Serial.println(cod);
+    delay(10000);
     }
 }
 
