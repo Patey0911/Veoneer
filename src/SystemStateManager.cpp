fdae1e1a12df46139143eff007be5c29b2f44929
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2022-05-22 21:48:21 +0300

    Update SystemStateManager.cpp

--- src/SystemStateManager.cpp
+++ src/SystemStateManager.cpp
@@ -2,25 +2,26 @@
 char *actual_state=NULL;
 void SystemStateManager_init()
 {
-    *actual_state=NULL;
+    actual_state=(char*)malloc(15);
+    strcpy(actual_state,"0");
 }
 
 void SystemStateManager_GetSystemState(char *actual_state_p)
 {
     SystemStateManager_MainFunction();
-  actual_state_p=actual_state;
-  if(actual_state!=NULL)
+  //actual_state_p=actual_state;
+  if(strcmp(actual_state_p,"1"))
     {
-        Serial.println(actual_state);
+        Serial.println(*actual_state_p);
     }
 }
 
 void SystemStateManager_MainFunction()
 { 
-    if(returnSTATUS()==1)
+    if(returnSTATUS()==true)
     {
         actual_state=(char*)malloc(sizeof(1));
         memcpy(actual_state,"1",1);
-        SystemStateManager_GetSystemState(actual_state);
     }
+    SystemStateManager_GetSystemState(actual_state);
 }
